11a880d35d10085a38d5e8fe083bc9c398354061
Jenny Radziwolek <contact@regxelerator.com>
2025-04-07 16:19:45 +0400

    Update Use Case 8

--- use_case_1/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-   """
-    When this file is executed, the code will run in the following sequence:
-
-    - Choose a classification technique (few_Shot_classifier.py, fine_tuning.py, or vector_embedding.py).
-    """
-
-if __name__ == "__main__":
-    main()
